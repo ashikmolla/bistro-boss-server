e143f53d3b271303f39ceaccdeb39ed338b07043
AsHiK-M(C.D) <ashik018361@gmaill.com>
2024-02-11 23:05:57 +0600

    add jwt token

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express=require('express');
-const app =express();
-const cors =require('cors');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
-const port =process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
@@ -32,49 +33,95 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const  menuCollection = client.db('bistroDb').collection('menu');
-    const  reviewsCollection = client.db('bistroDb').collection('reviews');
-    const  cartCollection = client.db('bistroDb').collection('carts');
-
-    //  multipol data cline send 
-  app.get('/menu', async(req, res)=>{
-    const result= await menuCollection.find().toArray();
-    res.send(result);
-  });
+    const usersCollection = client.db('bistroDb').collection('users');
+    const menuCollection = client.db('bistroDb').collection('menu');
+    const reviewsCollection = client.db('bistroDb').collection('reviews');
+    const cartCollection = client.db('bistroDb').collection('carts');
 
 
-  app.get('/reviews', async(req, res)=>{
-    const result= await reviewsCollection.find().toArray();
-    res.send(result);
-  });
+
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send(token)
+    })
 
 
-  // carts collection
-  
-  app.get('/carts', async(req, res)=>{
-    const email=req.query.email;
-    if(!email){
-      res.send([]);
-    }
-    const query= {email: email};
-    const result= await cartCollection.find(query).toArray();
-    res.send(result)
-    
-  })
 
-  app.post('/carts', async(req, res)=>{
-    const item=req.body;
-    // console.log(item);
-    const result=await cartCollection.insertOne(item);
-    res.send(result)
-  })
+    // user coulet in addmin 
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
 
-  app.delete('/carts/:id', async(req, res)=>{
-    const id=req.params.id;
-    const query={_id: new ObjectId(id)};
-    const result=await cartCollection.deleteOne(query);
-    res.send(result)
-  })
+    //USERS RELATED APIS
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user Already exists' })
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result)
+    })
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: `admin`
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result)
+
+
+    })
+
+
+
+
+    //  MENU RELATED APIS
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+
+    // REVIEWS RELATED APIS
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
+
+
+    // CARTS REALATED APIS
+
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result)
+
+    })
+
+    app.post('/carts', async (req, res) => {
+      const item = req.body;
+      // console.log(item);
+      const result = await cartCollection.insertOne(item);
+      res.send(result)
+    })
+
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result)
+    })
 
 
     // Send a ping to confirm a successful connection
@@ -93,9 +140,9 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res)=>{
-    res.send('Bistro is runnning')
+app.get('/', (req, res) => {
+  res.send('Bistro is runnning')
 })
-app.listen(port,()=>{
-    console.log(`Bistro boss is Running${port}`)
+app.listen(port, () => {
+  console.log(`Bistro boss is Running${port}`)
 })
